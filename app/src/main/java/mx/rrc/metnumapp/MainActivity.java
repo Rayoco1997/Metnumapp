11ea65d42f5b1024f4f3beb515d886b9e8060778
Rodrigo Nvz <a01377566@itesm.mx>
2017-11-25 21:08:25 -0600

    Botones

--- app/src/main/java/mx/rrc/metnumapp/MainActivity.java
+++ app/src/main/java/mx/rrc/metnumapp/MainActivity.java
@@ -2,12 +2,36 @@ package mx.rrc.metnumapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 //Probando el primer commit
+    ImageView metodo1rodrigo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Rodrigo
+        //Listener para botón
+        metodo1rodrigo = (ImageView) findViewById(R.id.metodo1rodrigo);
+        metodo1rodrigo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                finish();
+            }
+        });
+
+        //Raymundo
+
+
+
+        //Chema
+
+
+
     }
 }
